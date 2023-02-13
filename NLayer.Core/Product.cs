249d3858b1f78de962da5e4dc1f430ede79a1766
serhatyagci <serhatyagci1@gmail.com>
2023-02-13 23:24:28 +0300

    Adding core layer and creating interfaces

---
+++ NLayer.Core/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Core
+{
+    public class Product:BaseEntity
+    {
+        //Null olmayacak ise ctor yazılır ve nullexception hatası atar.
+        /*public Product(string name)
+        {
+            name=Name ?? throw new ArgumentNullException(nameof(Name));
+        }*/
+        public string Name { get; set; }
+        public int Stock { get; set; }
+        public decimal Price { get; set; }
+        public int CadegoryId { get; set; } //[ForeignKey("CategoryID")]
+        public Category Category { get; set; }  //Navigation propertyler
+        public ProductFeature ProductFeature { get; set; }  //Navigation propertyler
+    }
+}
---
+++ NLayer.Core/Services/Iservice.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq.Expressions;
+
+namespace NLayer.Core.Services
+{
+    public interface Iservice<T> where T : class
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<IEnumerable<T>> GetAllAsync();
+        IQueryable<T> where(Expression<Func<T, bool>> expression);
+        Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
+        Task AddAsync(T entity);
+        Task AddRangeAsync(IEnumerable<T> entities);
+        Task UpdateASync(T entity);
+        Task RemoveAsync(T entity);
+        Task RemoveRangeAsync(IEnumerable<T> entities);
+    }
+}
