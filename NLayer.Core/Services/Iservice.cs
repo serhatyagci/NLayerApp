a8797670ccd4ca77a49205b7e8b7233be50a54d2
serhatyagci <serhatyagci1@gmail.com>
2023-02-28 23:59:36 +0300

    add service layer

--- NLayer.Core/Services/Iservice.cs
+++ NLayer.Core/Services/Iservice.cs
@@ -9,7 +9,7 @@ namespace NLayer.Core.Services
         IQueryable<T> where(Expression<Func<T, bool>> expression);
         Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
         Task<T> AddAsync(T entity);
-        Task AddRangeAsync(IEnumerable<T> entities);
+        Task<IEnumerable<T>> AddRangeAsync(IEnumerable<T> entities);
         Task UpdateASync(T entity);
         Task RemoveAsync(T entity);
         Task RemoveRangeAsync(IEnumerable<T> entities);
