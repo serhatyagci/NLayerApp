aa22b956abd7515bf898bc52376e331970e4557b
serhatyagci <serhatyagci1@gmail.com>
2023-03-01 16:00:35 +0300

    Add CustomResponseDto

---
+++ NLayer.Core/DTOs/CustomResponseDto.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace NLayer.Core.DTOs
+{
+    public class CustomResponseDto<T>
+    {
+        public T Data { get; set; }
+
+        [JsonIgnore] //kodda lazım ancak clienta dönmesine gerek yok.
+        public int StatusCode { get; set; }
+
+        public List<String> Error { get; set; }
+
+
+        // static faktory metot design pattern (sınıf içerisinde nesne dönüşleri yapılıyor.)
+        public static CustomResponseDto<T> Success(int statusCode, T data)
+        {
+            return new CustomResponseDto<T> { Data = data, StatusCode = statusCode };
+        }
+        public static CustomResponseDto<T> Success(int statusCode)
+        {
+            return new CustomResponseDto<T> { StatusCode = statusCode };
+        }
+        public static CustomResponseDto<T> Fail(int statusCode, List<string> errors)
+        {
+            return new CustomResponseDto<T> { StatusCode = statusCode, Error = errors };
+        }
+        public static CustomResponseDto<T> Fail(int statusCode, string error)
+        {
+            return new CustomResponseDto<T> { StatusCode = statusCode, Error = new List<string> { error } };
+        }
+    }
+}
