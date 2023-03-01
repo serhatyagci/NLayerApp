1f8e48227bf9adbced80b1d586216acb89863d75
serhatyagci <serhatyagci1@gmail.com>
2023-03-01 14:34:24 +0300

    Add DTOs

--- NLayer.Core/Models/Product.cs
+++ NLayer.Core/Models/Product.cs
@@ -5,9 +5,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace NLayer.Core
+namespace NLayer.Core.Models
 {
-    public class Product:BaseEntity
+    public class Product : BaseEntity
     {
         //Null olmayacak ise ctor yazılır ve nullexception hatası atar.
         /*public Product(string name)
